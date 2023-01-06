643c0858c47d9e7aeda5c83580294c427828e0ea
kimtaehyeun <rlaxogus2799@naver.com>
2023-01-06 10:42:36 +0900

    StringEx2_teacher

--- src/com/iu/api1/Strings/StringEx2.java
+++ src/com/iu/api1/Strings/StringEx2.java
@@ -11,11 +11,22 @@ public class StringEx2 {
 		System.out.println("파일명을 입력");
 		String fileName = sc.next();//sf.sd.dsf.df
 		int idx = fileName.lastIndexOf(".");
-		System.out.println(idx);
+//		System.out.println(idx);
 		String result  = fileName.substring(idx+1);
-		System.out.println(result);
-			
-
+//		System.out.println(result);
+		boolean flag = true;
+		for(int i= 0; i<names.length;i++) {
+			if(result.equals(names[i])) {
+				System.out.println("정상 파일이다");
+				flag=!flag;
+				break;
+			}
+		
+		}
+		if(flag) {
+			System.out.println("비정상 파일이다.");
+		}
+		System.out.println("Finish");
 	}
 
 }
