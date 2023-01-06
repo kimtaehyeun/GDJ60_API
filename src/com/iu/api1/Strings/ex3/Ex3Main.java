d60bc3424745de5c40eadf21649a4ea9dc1ff2f4
kimtaehyeun <rlaxogus2799@naver.com>
2023-01-06 11:35:48 +0900

    1

---
+++ src/com/iu/api1/Strings/ex3/Ex3Main.java
@@ -0,0 +1,12 @@
+package com.iu.api1.Strings.ex3;
+
+public class Ex3Main {
+
+	public static void main(String[] args) {
+		System.out.println("Start");
+		
+		System.out.println("Finish");
+		
+	}
+
+}
