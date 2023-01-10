97f5f895ac431fe2e809714faecfaee0e6666642
kimtaehyeun <rlaxogus2799@naver.com>
2023-01-10 21:59:49 +0900

    home

--- src/com/iu/api2/util/RandomMain.java
+++ src/com/iu/api2/util/RandomMain.java
@@ -1,6 +1,8 @@
 package com.iu.api2.util;
 
 import java.util.ArrayList;
+import java.util.HashSet;
+
 import java.util.Random;
 
 public class RandomMain {
@@ -11,32 +13,67 @@ public class RandomMain {
 
 		//1000원 로또 구매 1-45
 		//로또 뽑는데 번호 총 6개 중복제거
-
-		//		int num = random.nextInt(9)+1;
-		ArrayList<Integer> rotto = new ArrayList<>();
-
-
-
-		for(int i =0; i<6; i++) {
-			rotto.add(random.nextInt(10)+1);
-			for(int j =0; j<rotto.size()-1;j++) {
-				
-				if(rotto.get(j)==rotto.get(j+1)) {
-					rotto.remove(j);
-					rotto.add(j,random.nextInt(10)+1);
-				}
-			
-			}
-		
+		HashSet<Integer>lotto= new HashSet<>();
+		while(lotto.size()<5) {
+			lotto.add(random.nextInt(10)+1);
 		}
-		rotto.remove(5);
+		System.out.println("set : "+lotto);
 
-
-
-		System.out.println(rotto);
-
-
-		//		System.out.println(num);
+		ArrayList<Integer> rotto = new ArrayList<>();
+		ArrayList<Integer> ar = new ArrayList<>();
+		for(int i =0; i<5; i++) {
+			rotto.add(random.nextInt(10)+1);
+		}
+		boolean sw = true;
+		int x = 0;
+//		while(sw) {
+//			
+//			for(int i=1; i<rotto.size(); i++) {
+//				if(rotto.get(0)==rotto.get(i)) {
+//					rotto.set(i,random.nextInt(10)+1);
+//				}
+//				else {
+//					for(int j=2;j<rotto.size();j++) {
+//						if(rotto.get(1)==rotto.get(j)) {
+//							rotto.set(j,random.nextInt(10)+1);
+//						}
+//						else{
+//							for(int a=3;a<rotto.size();a++) {
+//								if(rotto.get(1)==rotto.get(a)) {
+//									rotto.set(a,random.nextInt(10)+1);
+//								}
+//								else {
+//									for(int b=4;b<rotto.size();b++) {
+//										if(rotto.get(1)==rotto.get(b)) {
+//											rotto.set(b,random.nextInt(10)+1);
+//										}
+//										else
+//											sw=false;
+//									}
+//								}
+//							}
+//						}
+//					}
+//
+//				}
+//			}
+//
+//
+//			System.out.println("array : "+rotto);
+//			//		System.out.println(num);
+//		}
+		for(int i=1; i<7; i++) {
+			int num1 = random.nextInt(6)+1;
+			ar.add(num1);
+			for(int k=0; k<i-1;k++) {
+				
+				if(num1 == ar.get(k)) {
+					ar.remove(k);
+					i--;
+					break;
+				}
+			}
+		}
+		System.out.println(ar);
 	}
-
 }
